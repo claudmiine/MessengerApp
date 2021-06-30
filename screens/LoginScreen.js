c42433b2107e3ebcf57274924a1981d4b95e51ba
claudmiine <wojcieska.klaudia@gmail.com>
2021-06-30 14:21:04 +0200

    login page implemented

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import {  Stylesheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
 
 const LoginScreen = () => {
     return (
-      <View>
-        <Text> I am the login screen </Text>
-      </View>
+        <View>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+                }}
+                style={{ width: 200, height: 200 }}
+            />
+            <View style={styles.inputContainer}>
+            </View>
+            <Input placeholder="Email" autoFocus type="email" />
+            <Input placeholder="Password" secureTextEntry type="password" />
+        </View>
     );
-  }
+}
 
 
 export default LoginScreen;
 
-// const styles = Stylesheet.create({});
+const styles = StyleSheet.create({
+    inputContainer: { 
+    
+    },
+
+});
