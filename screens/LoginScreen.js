09880fc0071f0a6a551a8487a7c3c99709e2e64b
claudmiine <wojcieska.klaudia@gmail.com>
2021-06-30 13:32:59 +0200

    Style to header added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,1 +1,15 @@
-rnfe
+import React from 'react';
+import {  Stylesheet, View, Text } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+      <View>
+        <Text> I am the login screen </Text>
+      </View>
+    );
+  }
+
+
+export default LoginScreen;
+
+// const styles = Stylesheet.create({});
